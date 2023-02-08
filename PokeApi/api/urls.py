946e1d253742b40974e4c4d71bb9b04ff760cddc
Marvin <taipe.marvin17@gmail.com>
2023-02-08 12:03:32 -0500

    subir proyecto PokeApi

---
+++ PokeApi/api/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from .views import PokemonView,PokedexView
+
+
+urlpatterns = [
+    path('pokemon/',PokemonView.getPokemones,name='lista_pokemones'),
+    path('pokemon/<int:id>',PokemonView.getPokemonId,name='id_pokemon'),
+    path('pokemon/<nombre>',PokemonView.getPokemonNombre,name='nombre_pokemon'),
+    path('pokedex/',PokedexView.getPokedex,name='lista_pokedex'),
+    path('pokedex/<int:id>',PokedexView.getPokedexId, name='id_pokedex'),
+    path('pokedex/<nombre>',PokedexView.getPokedexNombre,name='nombre_pokedex')
+]
